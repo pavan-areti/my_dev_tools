ef380abfe7182a5a03e3ace2abf87d0452542184
pavanareti02 <pavanareti02@gmail.com>
2024-05-11 00:22:28 +0530

    feat: added code to open in new tab, added query params,added iframe

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/server-redirect', (req, res) => {
-    res.redirect('redirect-chain-1');
+    const ssr_page_id = req.query.ssr_page_id;
+    res.redirect(`redirect-chain-1?ssr_page_id=${ssr_page_id}`);
 });
 
 app.get('/redirect-chain-1', (req, res) => {
@@ -24,17 +25,21 @@ app.get('/redirect-chain-2', (req, res) => {
     res.redirect("https://www.google.com");
 });
 
-app.get('/client-redirect',(req,res)=>{
-    res.sendFile(__dirname+'/client-redirect.html');
+app.get('/client-redirect', (req, res) => {
+    res.sendFile(__dirname + '/client-redirect.html');
 });
 
-app.get('/meta-redirect',(req,res)=>{
-    res.sendFile(__dirname+'/meta-redirect.html');
+app.get('/meta-redirect', (req, res) => {
+    res.sendFile(__dirname + '/meta-redirect.html');
+});
+
+app.get('/iframe.html', (req, res) => {
+    res.sendFile(__dirname + '/iframe.html');
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
 
 export default app;
